f148a9a02739112e48e179397ec0104a829b4a72
Cvelthoven <cjv.velthoven@gmail.com>
2022-02-13 15:32:53 +0100

    contructor adjusted

--- WinLibRegistry/WinLibRegistry/LibRegistry.cpp
+++ WinLibRegistry/WinLibRegistry/LibRegistry.cpp
@@ -1,2 +1,43 @@
+//---------------------------------------------------------------------------------------
+//
+//	LibRegistry
+//
+//---------------------------------------------------------------------------------------
 #include "pch.h"
 #include "LibRegistry.h"
+
+#include <string>
+
+
+
+//---------------------------------------------------------------------------------------
+//
+//	LibRegistry constructors
+//
+//---------------------------------------------------------------------------------------
+LibRegistry::LibRegistry(
+	string& strAplicationDomain,
+	string& strAplicationName)
+{
+	strAplDomain = strAplicationDomain;
+	strAplicationName = strAplicationName;
+
+}
+
+//---------------------------------------------------------------------------------------
+//
+//	Public class methodes
+//
+//---------------------------------------------------------------------------------------
+//
+//	GetRegistryKeyValue
+//
+//---------------------------------------------------------------------------------------
+int LibRegistry::GetRegistryKeyValue(
+	const string& strSection,
+	const string& strKey,
+	string& strRegKeyValue)
+{
+	return 0;
+}
+
--- WinLibRegistry/WinLibRegistry/LibRegistry.h
+++ WinLibRegistry/WinLibRegistry/LibRegistry.h
@@ -1,5 +1,49 @@
+//---------------------------------------------------------------------------------------
+//
+//	LibRegistry.h
+//
+//---------------------------------------------------------------------------------------
 #pragma once
-class LibRegistry
-{
-};
 
+#ifdef LIBREGISTRY_EXPORTS
+#define LIBREGISTRY __declspec(dllexport)
+#else
+#define LIBREGISTRY __declspec(dllimport)
+#endif
+
+//---------------------------------------------------------------------------------------
+//
+//	Header files
+//
+//---------------------------------------------------------------------------------------
+using namespace std;// needed for default header files
+#include <string>
+
+class LibRegistry 
+{
+//---------------------------------------------------------------------------------------
+//
+//	Public declarations
+//
+//---------------------------------------------------------------------------------------
+public:
+	LibRegistry(
+		string& strAplicationDomain,
+		string& strAplicationName);
+
+	int GetRegistryKeyValue(
+		const string& strSection,
+		const string& strKey,
+		string& strRegKeyValue);
+
+//---------------------------------------------------------------------------------------
+//
+//	Private declarations
+//
+//---------------------------------------------------------------------------------------
+private:
+	string
+		strAplDomain,
+		strAplName;
+
+};
