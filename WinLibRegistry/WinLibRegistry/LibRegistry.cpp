aa6bbafc82567f04b78f49f4652d2a3412cd8b12
Cvelthoven <cjv.velthoven@gmail.com>
2022-02-14 11:36:00 +0100

    Prep getResistryKeyValue

--- WinLibRegistry/WinLibRegistry/LibRegistry.cpp
+++ WinLibRegistry/WinLibRegistry/LibRegistry.cpp
@@ -33,11 +33,34 @@ LibRegistry::LibRegistry(
 //	GetRegistryKeyValue
 //
 //---------------------------------------------------------------------------------------
+//
+//	GetRegistryKeyValue returning a sting value
+//
+//---------------------------------------------------------------------------------------
 int LibRegistry::GetRegistryKeyValue(
 	const string& strSection,
 	const string& strKey,
 	string& strRegKeyValue)
 {
+	//-----------------------------------------------------------------------------------
+	//
+	//	Build registry path
+	//
+	strRegistryKeyPath = "SOFTWARE\\" + strAplDomain + "\\" + strAplName;
+	if (strSection.length() > 0)
+		strRegistryKeyPath = strRegistryKeyPath + "\\" + strSection;
+
 	return 0;
 }
+
+//---------------------------------------------------------------------------------------
+//
+//	Private class methodes
+//
+//---------------------------------------------------------------------------------------
+//
+//	GetRegistryKeyValue
+//
+//---------------------------------------------------------------------------------------
+
 
--- WinLibRegistry/WinLibRegistry/LibRegistry.h
+++ WinLibRegistry/WinLibRegistry/LibRegistry.h
@@ -44,6 +44,7 @@ public:
 private:
 	string
 		strAplDomain,
-		strAplName;
+		strAplName,
+		strRegistryKeyPath;
 
 };
