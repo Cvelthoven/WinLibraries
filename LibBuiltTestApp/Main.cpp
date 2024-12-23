25922d4252f3fca20f857fcf943a7412464d50f6
Cvelthoven <cjv.velthoven@gmail.com>
2024-12-23 15:43:25 +0100

    Main class add
    Base for testing functions of WinLib

---
+++ LibBuiltTestApp/Main.cpp
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------------------
+//
+// Module: Main.cpp
+//
+// This is the entry point for building and testing the WinLibrary classes.
+//
+//---------------------------------------------------------------------------------------
+#include "Main.h"
+
+int main()
+{
+	return 0;
+}
---
+++ LibBuiltTestApp/Main.h
@@ -0,0 +1,15 @@
+#pragma once
+//---------------------------------------------------------------------------------------
+// Main.h
+// 
+// This is the main header file for the application of the main window.
+// This file contains the class definition for the main window.
+// 
+//---------------------------------------------------------------------------------------
+class Main
+{
+public:
+	Main();
+	~Main();
+};
+
